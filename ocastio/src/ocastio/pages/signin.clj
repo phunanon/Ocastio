f86e2cdc70a0a73126c19c765f38b3e0ab385ffe
Patrick Bowen <phunanon@users.noreply.github.com>
2019-07-23 02:04:15 +0100

    Fix sign-in errors not showing

--- ocastio/src/ocastio/pages/signin.clj
+++ ocastio/src/ocastio/pages/signin.clj
@@ -12,7 +12,9 @@
   :email-invalid  "The email supplied is invalid."})
 
 ; TODO don't allow empty username, password
-(defn page [{{sess :session redir :redir :as para} :params :as request}
+(defn page [{{redir :redir :as para} :params
+             sess                    :session
+             :as request}
             compose]
   (let [action    (if (nil? redir) "/sign" (str "/sign?redir=" redir))
         sign-err  (:sign-err sess)]
@@ -21,7 +23,7 @@
       (if (some? sign-err) [:b (sign-err error-messages)])
       [:form {:action action :method "POST"}
         (util/anti-forgery-field)
-        [:input {:type "text"     :placeholder "Email" :name "email" :value (:form-email sess)}]
+        [:input {:type "text"     :placeholder "Email" :name "email"}]
         [:input {:type "password" :placeholder "Password" :name "password"}]
         [:input {:type "submit"   :value "Sign in" :name "signin"}]
         [:input {:type "submit"   :value "Register" :name "register"}]])))
@@ -30,7 +32,7 @@
   (let [signin?   (nil? (:register para))
         exists?   (db/email-exists? email)
         session   (:session request)
-        session   (dissoc session :sign-err :form-email)
+        session   (dissoc session :sign-err)
         redirect  (:redir para)
         nextdir   (if (nil? redirect) "/" redirect)
         return    (fn [redir sess] {:redir redir :sess (into session sess)})]
@@ -39,13 +41,13 @@
         (if exists?
           (if (db/correct-pass? email pass)
             (return nextdir   {:email email})
-            (return "/signin" {:sign-err :pass-incorrect :form-email email}))
-          (return "/signin" {:sign-err :email-nonexist :form-email email}))
+            (return "/signin" {:sign-err :pass-incorrect}))
+          (return "/signin" {:sign-err :email-nonexist}))
         (if exists?
-          (return "/signin" {:sign-err :email-exists :form-email email})
+          (return "/signin" {:sign-err :email-exists})
           (do (db/new-user! email pass)
               (return nextdir {:email email}))))
-      (return "/signin" {:sign-err :email-invalid :form-email email}))))
+      (return "/signin" {:sign-err :email-invalid}))))
 
 (defn signout [{{redir :redir} :params}]
   {:redir (if redir redir "/") :sess {}})
